012b6e72baa3022ca3d35a3cb39e472e0e641309
RisheeA <rishee.sds@gmail.com>
2026-03-09 09:58:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // =============================================================================
 // CONFIGURATION
